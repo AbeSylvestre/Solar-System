5c18b7683e9f133f0d3682d01a1bd0deaec332b2
AbeSylvestre <abraham.sylvestre@gmail.com>
2016-03-15 16:10:55 -0400

    Added initial files for scrolling

--- js/script.js
+++ js/script.js
@@ -15,32 +15,4 @@ $(document).ready(function() {
         return false;
     });
 
-    //Section elements animation
-        //Thanks to Simon Codrington for animation guide
-    var $animation_elements = $("animation_element");
-    var $window = $("window");
-    $window.trigger("scroll");
-    $window.on("scroll resize", check_if_in_view);
-    $(".onScreen").fadeIn();
-
-    function check_if_in_view() {
-        var windowHeight = $window.height();
-        var windowTopPosition = $window.scrollTop();
-        var windowBottomPosition = (windowTopPosition + windowHeight);
-
-        $.each($animation_elements, function() {
-            var $element = $(this);
-            var element_height = $element.outerHeight();
-            var elementTopPosition = $element.offset().top;
-            var elementBottomPosition = (elementTopPosition + element_height);
-
-            //check to see if this current container is within viewport
-            if ((elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition)) {
-                $element.addClass('onScreen');
-            } else {
-                $element.removeClass('onScreen');
-            }
-        });
-    }
-
 });
