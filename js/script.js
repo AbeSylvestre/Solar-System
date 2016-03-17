ca974d751835a3ec8c04c48b5eb60af2fb0cf42b
AbeSylvestre <abraham.sylvestre@gmail.com>
2016-03-16 22:02:42 -0400

    Initial Live edition
    Added missing viewport metatag; removed initial scroll button appearance
    on small screens.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $(window).scroll(function() {
-        if($(this).scrollTop() > 100) {
+        if($(window).width() > 525 && $(this).scrollTop() > 100) {
             $(".scrollBtn").fadeIn();
         }
         else {
@@ -14,33 +14,5 @@ $(document).ready(function() {
         $("html, body").animate({scrollTop : 0}, 800);
         return false;
     });
-
-    //Section elements animation
-        //Thanks to Simon Codrington for animation guide
-    var $animation_elements = $("animation_element");
-    var $window = $("window");
-    $window.trigger("scroll");
-    $window.on("scroll resize", check_if_in_view);
-    $(".onScreen").fadeIn();
-
-    function check_if_in_view() {
-        var windowHeight = $window.height();
-        var windowTopPosition = $window.scrollTop();
-        var windowBottomPosition = (windowTopPosition + windowHeight);
-
-        $.each($animation_elements, function() {
-            var $element = $(this);
-            var element_height = $element.outerHeight();
-            var elementTopPosition = $element.offset().top;
-            var elementBottomPosition = (elementTopPosition + element_height);
-
-            //check to see if this current container is within viewport
-            if ((elementBottomPosition >= windowTopPosition) && (elementTopPosition <= windowBottomPosition)) {
-                $element.addClass('onScreen');
-            } else {
-                $element.removeClass('onScreen');
-            }
-        });
-    }
 
 });
